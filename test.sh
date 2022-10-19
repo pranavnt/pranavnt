2093155cc9e7b83e219a291243c721fbd55daaa3
Pranav Teegavarapu <46251241+pranavnt@users.noreply.github.com>
2022-10-19 11:14:59 -0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,4 @@
+ln -s /Volumes/SDL2/SDL2.framework/SDL2 /usr/lib/libSDL2
+ln -s /Volumes/SDL2_tff/SDL2_ttf.framework/SDL2_ttf /usr/local/lib/SDL2_ttf
+ln -s /Volumes/SDL2_mixer/SDL2_mixer.framework/SDL2_mixer /usr/local/lib/libSDL2_mixer
+ln -s /Volumes/SDL2_image/SDL2_image.framework/SDL2_mixer /usr/local/lib/libSDL2_image
